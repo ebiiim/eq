fc901ad2f3d438a3d15f01fdf6ec8e56184cdfd1
ebiiim <mail@ebiiim.com>
2019-07-07 02:15:56 +0900

    docs: internal/safe

--- internal/safe/buffer.go
+++ internal/safe/buffer.go
@@ -1,3 +1,4 @@
+// Package safe provides thread-safe objects for internal use.
 package safe
 
 import (
@@ -5,23 +6,27 @@ import (
 	"sync"
 )
 
+// Buffer provides a thread-safe bytes.Buffer.
 type Buffer struct {
 	buf bytes.Buffer
 	mu  sync.Mutex
 }
 
+// Read provides a thread-safe bytes.Buffer.Read function.
 func (s *Buffer) Read(p []byte) (n int, err error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	return s.buf.Read(p)
 }
 
+// Write provides a thread-safe bytes.Buffer.Write function.
 func (s *Buffer) Write(p []byte) (n int, err error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	return s.buf.Write(p)
 }
 
+// Len provides a thread-safe bytes.Buffer.Len function.
 func (s *Buffer) Len() int {
 	s.mu.Lock()
 	defer s.mu.Unlock()
